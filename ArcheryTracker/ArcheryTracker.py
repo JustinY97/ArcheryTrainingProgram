06f163a0dec9b7fe538c159d044fa59713953f3a
Justin York <justin120197@gmail.com>
2021-08-18 15:57:10 -0400

    Added editEvent function need to add a menu to it so that you can actually edit it!!

--- ArcheryTracker/ArcheryTracker.py
+++ ArcheryTracker/ArcheryTracker.py
@@ -11,11 +11,35 @@ def clear():
      os.system('cls' if os.name=='nt' else 'clear')
      return("   ")
 
+def printEvents(events, eventNums=True):
+    eventList = []
+    for val in list(events.get("3D Events")):
+        eventList.append(val.get("Title"))
+    for val in list(events.get("Indoor Events")):
+        eventList.append(val.get("Title"))            
+    for val in list(events.get("Outdoor Events")):
+        eventList.append(val.get("Title"))
+
+    eventList.sort()
+    if eventNums:
+        i = 1
+        for val in eventList:
+            print("%d. %s" %(i, val))
+            i+= 1
+    else:
+        for val in eventList:
+            print(val)
+
+    return eventList
+
 def add3DEvent(lst):
     title = input("Enter Title of Event: ")
     newEvent = {"Title" : title, "Date" : str(date.today()), "Targets" : {}}
     lst.append(newEvent)
     events.update({"3D Events" : lst})
+
+def add3DTarget(lst):
+    print(lst)
 
 def addIndoorEvent(lst):
     title = input("Enter Title of Event: ")
@@ -29,14 +53,21 @@ def addOutdoorEvent(lst):
     lst.append(newEvent)
     events.update({"Outdoor Events" : lst})
 
-### MAIN ###
+def editEvent(event):
+    print("Inside Function")
+    sleep(1)
+
+
+    ### MAIN ###
 events = json.load(open('ArcheryTracker\Data\events.json', 'r+'))
+
 while True:
     print("Select Choice")
     print("1. Add Event")
     print("2. View Event")
     print("3. Edit Event")
     print("4. Exit")
+    print("5. Test")
     user = int(input())
     clear()
 
@@ -58,26 +89,44 @@ while True:
 
     ### VIEW A SPECIFIC EVENT ###
     elif user == 2:
-        eventList = []
-        for val in list(events.get("3D Events")):
-            eventList.append(val.get("Title"))
-        for val in list(events.get("Indoor Events")):
-            eventList.append(val.get("Title"))
-        for val in list(events.get("Outdoor Events")):
-            eventList.append(val.get("Title"))
-
-        eventList.sort()
-        for val in eventList:
-            print(val)
-        
+        printEvents(events)
+            
         ### PICK AN EVENT TITLE AND FIND THAT EVENT INFO ###
+
+        ### ASK TO SAVE INFO AND SAVE ###
+
+
+    ### EDIT AN EVENT ###
+    elif user == 3:
+
+        ### PRINT LIST OF EVENTS WITH NUMBERS AND RETURN THE EVENT INFO ###
+        lst = printEvents(events)
+        eventNum = int(input("Event Number: "))
+        clear()
+        eventToEdit = {}
+
+        ### GOES THROUGH THE DIFFERENT EVENT TYPES ###
+        for i in events.values():
+            ### GOES THROUGH EACH EVENT IN THE EVENT TYPE ###
+            for j in i:
+                if j.get("Title") == lst[eventNum - 1]:
+                    eventToEdit = j
         
+        print("Going To Function")
+        editEvent(eventToEdit)
         
-    ### QUIT ###
+        clear()
+
+        
+            
+
     elif user == 4:
         print("Exiting and Saving...")
-        sleep(2)
+        sleep(1.5)
         clear()
         break
 
-json.dump(events, open('Data/events.json', 'r+'), indent=2)
+        
+
+json.dump(events, open('ArcheryTracker/Data/events.json', 'r+'), indent=2)
+
