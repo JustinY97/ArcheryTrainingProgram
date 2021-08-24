b0bbd338d40a5c37dbd80e9dbdddbbb1a888c325
Justin York <justin120197@gmail.com>
2021-08-24 12:57:24 -0400

    Made eventList list creation recursive and made a getEvent function that returns an event given an event Title

--- ArcheryTracker/ArcheryTracker.py
+++ ArcheryTracker/ArcheryTracker.py
@@ -11,6 +11,19 @@ def clear():
      os.system('cls' if os.name=='nt' else 'clear')
      return("   ")
 
+def menu():
+    print("Select Choice")
+    print("1. Add Event")
+    print("2. View Event")
+    print("3. Edit Event")
+    print("4. Exit")
+    user = int(input("Select choice...\n"))
+    clear()
+    return user
+
+def editEvent(lst):
+    print("Inside EditEvent Function")
+
 def add3DEvent(lst):
     title = input("Enter Title of Event: ")
     newEvent = {"Title" : title, "Date" : str(date.today()), "Targets" : {}}
@@ -29,24 +42,24 @@ def addOutdoorEvent(lst):
     lst.append(newEvent)
     events.update({"Outdoor Events" : lst})
 
-### MAIN ###
-events = json.load(open('Data/events.json', 'r+'))
-while True:
-    print("Select Choice")
-    print("1. Add Event")
-    print("2. View Event")
-    print("3. Edit Event")
-    print("4. Exit")
-    user = int(input())
-    clear()
+def getEvent(eventTitle):
+    for eventType in events.values():
+        for val in eventType:
+            return val
 
-    ### ADD A NEW EVENT ###
+### MAIN ###
+
+events = json.load(open('ArcheryTracker/Data/events.json', 'r+'))
+
+while True:
+    user = menu()
+
     if user == 1:
         print("Type of Event")
         print("1. 3D Event")
         print("2. Indoor Event")
         print("3. Outdoor Event")
-        event = int(input("Enter Choice: "))
+        event = int(input("Enter Choice...\n"))
         clear()
 
         if event == 1:
@@ -59,25 +72,44 @@ while True:
     ### VIEW A SPECIFIC EVENT ###
     elif user == 2:
         eventList = []
-        for val in list(events.get("3D Events")):
-            eventList.append(val.get("Title"))
-        for val in list(events.get("Indoor Events")):
-            eventList.append(val.get("Title"))
-        for val in list(events.get("Outdoor Events")):
-            eventList.append(val.get("Title"))
-
+        for val in events.values():
+            for evnt in val:
+                eventList.append(evnt.get("Title"))
+                
         eventList.sort()
-        for val in eventList:
-            print(val)
+        for i in range(eventList.__len__()):
+            print(str(i + 1) + ". " + eventList[i])
         
         ### PICK AN EVENT TITLE AND FIND THAT EVENT INFO ###
-        
-        
+        eventID = input("Enter Event Title or ID\n")
+        try:
+            eventID = int(eventID)
+            eventID = eventList[int(eventID)- 1]
+            clear()
+        except:
+            clear()
+
+        evnt = getEvent(eventID)
+        print(evnt)
+
     ### QUIT ###
     elif user == 4:
-        print("Exiting and Saving...")
-        sleep(2)
-        clear()
-        break
+        try:
+            save = int(input("1. Save\n2. Exit without saving\n"))
+            sleep(2)
+            clear()
+    
+            if save == 1:
+                json.dump(events, open('ArcheryTracker/Data/events.json', 'r+'), indent=2)
+            elif save == 2:
+                save = input("Are you sure you want to quit without saving! (Y/N)\n")
+                save = str.upper(save)
+                if save == "Y":
+                    print("Exiting without saving...")  
+                elif save == "N":
+                    print("Saving...")
+                    json.dump(events, open("ArcheryTracker/Data/events.json", "r+"), indent=2)
+        except(Exception):
+            print("Error in input")
 
-json.dump(events, open('Data/events.json', 'r+'), indent=2)
+        break
